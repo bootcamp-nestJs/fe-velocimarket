8d26905c44ee19917de3d45c74c282a5497d863d
Alicia Gallardo <ali.ale.gallardo@gmail.com>
2023-11-07 09:37:34 -0300

    portadabanner responsive

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,10 +1,10 @@
-import banner  from '../assets/img/bannerimg.png'
+
 import './banner.css'
 
 export function Banner() {
     return (
         <div className="banner">
-        <img src={banner} alt="" />
+        <img className='portada' alt="" />
     </div>
     )
 }
